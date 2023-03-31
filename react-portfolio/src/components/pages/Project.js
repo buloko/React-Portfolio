9a065c32efc6e610d8c559c3e4253a52a5144411
buloko <buloko8@gmail.com>
2023-03-31 15:07:53 -0400

    adding all project images and style to display

---
+++ react-portfolio/src/components/pages/Project.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import CodeQuiz from "../../../src/assets/images/buloko.github.io_Code-Quiz_.png";
+import PasswordGenerator from "../../../src/assets/images/buloko.github.io_Password-Generator_.png";
+import WeatherDashboard from "../../../src/assets/images/buloko.github.io_Weather-Dashboard_.png";
+import MarvelSearch from "../../../src/assets/images/evolangit.github.io_Marvel-Search-Engine_.png";
+import JATE from "../../../src/assets/images/glacial-retreat-87961.herokuapp.com_.png";
+import README from "../../../src/assets/images/Screen Shot 2023-03-31 at 2.40.12 PM.png";
+export default function Projects() {
+    
+return (
+    <div>
+        <div id= "project section" className="relative py-20 md:py-24 bg-gray-800">
+            
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
