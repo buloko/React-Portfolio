cf422ae53dd796c4fc5bd4faf38a97ed8ca830cd
buloko <buloko8@gmail.com>
2023-03-31 23:52:11 -0400

    pull everything into root of the app

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Resume from "../../../src/assets/Benjamin_Uloko_Resume.pdf"
+import Resume from "../../assets/Benjamin_Uloko_Resume.pdf"
 
 export default function Contact() {
     return (
